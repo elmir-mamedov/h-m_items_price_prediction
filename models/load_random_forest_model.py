e4642583e773bd93b82488a483a64bbe65569bb6
elmir-mamedov <mamedovelmir39@gmail.com>
2025-05-11 12:27:21 +0200

    Delete models/load_random_forest_model.py (reduntant)

--- models/load_random_forest_model.py
+++
@@ -1,9 +0,0 @@
-import json
-from sklearn.ensemble import RandomForestRegressor
-
-# 1. Load the saved parameters
-with open('random_forest_params.json', 'r') as f:
-    params = json.load(f)
-
-# 2. Create the model
-RandomForest_model = RandomForestRegressor(**params)
